2f17d2a59cdd925643c86b16e5e6710c51d40a13
Jose Fernando Bejarano Peña <josebejarano@cbit-online.com>
2021-01-25 08:35:25 -0500

    FB : Write response file

--- Bussines/BSManagement.cs
+++ Bussines/BSManagement.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Api.Domicile.Drone.Management.Bussines
@@ -13,7 +14,37 @@ namespace Api.Domicile.Drone.Management.Bussines
         // Globals
         DTODroneRoute route = new DTODroneRoute();
         List<string> ListRoutes = new List<string>();
-        string pathFile = AppDomain.CurrentDomain.BaseDirectory;
+        string pathFile = AppDomain.CurrentDomain.BaseDirectory.Replace(@"bin\Debug\net5.0", "ResultFiles");
+        string path = string.Empty;
+        int cont = 0;
+
+        public void FileManagement(DTORouteFileRequest routesFile)
+        {
+            foreach (var order in routesFile.route)
+            {
+                cont++;
+                ListRoutes = ReadFile(order);
+                if (ListRoutes != null)
+                {
+                    WritePathResult("out0" + cont);
+                    ValidatorRules(ListRoutes);
+                }
+            }
+        }
+
+        public void WritePathResult(string filename)
+        {
+            string result = string.Empty;
+            path = string.Format("{0}{1}.txt", pathFile, filename);
+            if (!Directory.Exists(path))
+                File.WriteAllText(path, "==Reporte de entregas==");
+        }
+
+        public void EditPathResult(string pathfile, string textResult)
+        {
+            if (!Directory.Exists(path))
+                File.AppendAllText(pathfile, Environment.NewLine + textResult);
+        }
 
         public List<string> ReadFile(IFormFile file)
         {
@@ -24,6 +55,41 @@ namespace Api.Domicile.Drone.Management.Bussines
             }
             return ListRoutes;
         }
+
+        public void ValidatorRules(List<string> routes)
+        {
+            string letter = string.Empty;
+            foreach (var item in routes)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    letter = item[i].ToString();
+                    if ((i == 0) && (route.CardinalPosition == null))
+                        route = ValidatorByLetter(MapInitialRoute(), letter);
+                    else if (i > 0 || (route.CardinalPosition != null))
+                        route = ValidatorByLetter(route, letter);
+                }
+                EditPathResult(path, string.Format("({0},{1}) {2}", route.XPosition, route.YPosition, HomologateCardinalPosition(route.CardinalPosition)));
+            }
+        }
+
+        public string HomologateCardinalPosition(string cardinalPosition)
+        {
+            switch (cardinalPosition)
+            {
+                case "N":
+                    return "Dirección Norte";
+                case "S":
+                    return "Dirección Sur";
+                case "E":
+                    return "Dirección Este";
+                case "O":
+                    return "Dirección Oeste";
+                default:
+                    return null;
+            }
+        }
+
 
         public DTODroneRoute ValidatorByLetter(DTODroneRoute route, string letter)
         {
@@ -43,16 +109,6 @@ namespace Api.Domicile.Drone.Management.Bussines
             return route;
         }
 
-        public DTODroneRoute MapInitialRoute()
-        {
-            return new DTODroneRoute()
-            {
-                CardinalPosition = "N",
-                XPosition = 0,
-                YPosition = 0
-            };
-        }
-
         public string CardinalPointValidatorToTheRight(string cardinalPoint)
         {
             switch (cardinalPoint)
@@ -68,6 +124,21 @@ namespace Api.Domicile.Drone.Management.Bussines
                 default: return null;
             }
 
+        }
+        public string CardinalPointValidatorToTheLeft(string cardinalPoint)
+        {
+            switch (cardinalPoint)
+            {
+                case "N":
+                    return "O";
+                case "O":
+                    return "S";
+                case "S":
+                    return "O";
+                case "E":
+                    return "N";
+                default: return null;
+            }
         }
 
         public DTODroneRoute MoveCardinalPointForward(DTODroneRoute route)
@@ -91,20 +162,14 @@ namespace Api.Domicile.Drone.Management.Bussines
             return route;
         }
 
-        public string CardinalPointValidatorToTheLeft(string cardinalPoint)
+        public DTODroneRoute MapInitialRoute()
         {
-            switch (cardinalPoint)
+            return new DTODroneRoute()
             {
-                case "N":
-                    return "O";
-                case "O":
-                    return "S";
-                case "S":
-                    return "O";
-                case "E":
-                    return "N";
-                default: return null;
-            }
+                CardinalPosition = "N",
+                XPosition = 0,
+                YPosition = 0
+            };
         }
     }
 }
