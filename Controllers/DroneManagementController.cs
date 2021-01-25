3ce081b27061d707f3d74ba4361b1fc63402f096
Jose Fernando Bejarano Peña <josebejarano@cbit-online.com>
2021-01-24 22:31:45 -0500

    FB ´: Controller definition - endPoint Rules

--- Controllers/DroneManagementController.cs
+++ Controllers/DroneManagementController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Api.Domicile.Drone.Management.DTO;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,18 @@ using System.Threading.Tasks;
 
 namespace Api.Domicile.Drone.Management.Controllers
 {
-    public class DroneManagementController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DroneManagementController : ControllerBase
     {
-        public IActionResult Index()
+        /// <summary>
+        /// </summary>
+        /// <param name="routeFile"></param>
+        /// <returns></returns>
+        [Route("PostReceiveRoutes")]
+        [HttpPost]
+        public async Task<IActionResult> PostReceiveRoutes([FromForm] DTORouteFileRequest routesFile)
         {
-            return View();
+            return Ok();
         }
     }
-}
