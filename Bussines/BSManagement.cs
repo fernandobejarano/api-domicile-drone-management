1e20347154121533d0382540896be0e45aa1a2e5
Jose Fernando Bejarano Peña <josebejarano@cbit-online.com>
2021-01-24 22:11:55 -0500

    Agregar archivos de proyecto.

---
+++ Bussines/BSManagement.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Domicile.Drone.Management.DTO
+{
+    public class DTORouteFileRequest
+    {
+        public List<IFormFile> route { get; set; }
+    }
+
+    public class DTODroneRoute
+    {
+        public string CardinalPosition { get; set; }
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+
+    }
+}
